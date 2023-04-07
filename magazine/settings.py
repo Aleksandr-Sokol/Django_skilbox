aa88432686a0690258a73f14ab29b307ae1ba477
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2023-04-07 16:13:27 +0500

    Добавлен свагер

--- magazine/settings.py
+++ magazine/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework_swagger',
     "product",
 ]
 
@@ -64,6 +65,8 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            'libraries':
+                {'staticfiles': 'django.templatetags.static', },
         },
     },
 ]
@@ -116,3 +119,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
--- magazine/urls.py
+++ magazine/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='API')
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("docs/", schema_view),
+    # path('api/', include('product.urls')),
 ]
