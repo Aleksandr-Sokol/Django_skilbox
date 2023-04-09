1b3bbfabeb005a0f6eeccac2ba7245e9698fc2ac
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2023-04-09 11:15:12 +0500

    Добавлена OTM аудентификация

--- magazine/settings.py
+++ magazine/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_otp',
+    'django_otp.plugins.otp_totp',
     'rest_framework_swagger',
     "product",
 ]
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_otp.middleware.OTPMiddleware',
 ]
 
 ROOT_URLCONF = "magazine.urls"
